858ebd5507830ed0838cddc1309ac8f3a79d84de
Sergei Sachkov <sergei.sachkov@ya.ru>
2020-05-06 17:07:40 -0400

    - added HeaderType - added HeaderValue

---
+++ src/main/java/response/HeaderType.java
@@ -0,0 +1,7 @@
+package response;
+
+public class HeaderType {
+    public static final String CONTENT_TYPE = "content-type";
+    public static final String AUTHORIZATION = "authorization";
+
+}
---
+++ src/main/java/response/HeaderValue.java
@@ -0,0 +1,6 @@
+package response;
+
+public class HeaderValue {
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String APPLICATION_JAVASCRIPT = "application/javascript";
+}
